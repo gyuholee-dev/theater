1ad705de8e78df938fb316adbfd7ace6e4bceb2d
LeeGyuho <andrewgyuholee@gmail.com>
2022-04-14 10:20:53 +0900

    기초 개발환경 셋업

---
+++ includes/start.php
@@ -0,0 +1,10 @@
+<?php
+
+// 헤드
+$head = file_get_contents('templates/head.html');
+
+// 헤더
+$header = file_get_contents('templates/header.html');
+
+// 푸터
+$footer = file_get_contents('templates/footer.html');
