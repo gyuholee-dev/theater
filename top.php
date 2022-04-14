71a2977b0655ce1733aa2b25c764f6d3abe3f4dd
LeeGyuho <andrewgyuholee@gmail.com>
2022-04-14 12:19:43 +0900

    스타트

---
+++ top.php
@@ -0,0 +1,34 @@
+<?php
+require_once 'includes/init.php';
+require_once 'includes/start.php';
+?>
+<!-- HTML START -->
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <?=$head?>
+    <!-- css 추가 -->
+    <!-- <link rel="stylesheet" href="styles/top.css"> -->
+    <!-- js 추가 -->
+    <!-- <script src="scripts/top.js"></script> -->
+</head>
+<body>
+    <header>
+        <?=$header?>
+    </header>
+    <main>
+    <!-- 여기에 컨텐츠를 추가
+        <section id="sectionId">
+            <div class="class type">
+                내용
+            </div>
+            ...
+        </section> 
+        ...
+    -->
+    </main>
+    <footer>
+        <?=$footer?>
+    </footer>
+</body>
+</html>
