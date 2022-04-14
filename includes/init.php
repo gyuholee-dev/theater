72e59ac60ce9fdbd32d107422c5c0d4246f4d69a
LeeGyuho <andrewgyuholee@gmail.com>
2022-04-14 10:37:54 +0900

    초기화 및 기초함수 추가

--- includes/init.php
+++ includes/init.php
@@ -1,1 +1,8 @@
-<?php
+<?php // init.php
+// 초기화 ------------------------------------------------
+require_once 'includes/functions.php';
+require_once 'includes/elements.php';
+ini_set('display_errors', 'On');
+ini_set('session.use_strict_mode', 0);
+mysqli_report(MYSQLI_REPORT_ALL);
+session_start();
