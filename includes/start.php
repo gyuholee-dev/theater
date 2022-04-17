9ca487abb2a34fe301c32b717facdcf6e44a31e8
LeeGyuho <andrewgyuholee@gmail.com>
2022-04-17 11:16:45 +0900

    Update start.php

--- includes/start.php
+++ includes/start.php
@@ -1,7 +1,23 @@
-<?php
+<?php // start.php
+
+// 데이터 선언
+$head = '';
+$message = '';
+$header = '';
+$nav = '';
+$content = '';
+$aside = '';
+$footer = '';
 
 // 헤드
-$head = file_get_contents('templates/head.html');
+$head_data = [
+    'title' => "$INFO[title] : $INFO[subtitle]",
+    'description' => "$INFO[description]",
+];
+$head = renderElement(TPL.'head.html', $head_data);
+
+// 메시지
+$message = printLog();
 
 // 헤더
 $header = file_get_contents('templates/header.html');
