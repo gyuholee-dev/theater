0316b8544aa0c030757e51f30286d05f0f5fa600
yeluyelu <99127003+yeluyelu@users.noreply.github.com>
2022-04-18 18:15:10 +0900

    공지사항 작업중 scss는 적고있는중

--- notice.php
+++ notice.php
@@ -26,7 +26,98 @@ require_once 'includes/start.php';
                     <li><a href="location.php">오시는길</a></li>
                 </ul>
             </nav>
-
+            <div class="moviebox">
+                <table class="theat">
+                    <tr>
+                        <td colspan="4" align="center"></td>
+                           <h3>공지사항</h3>
+                    </tr>
+                    <tr>
+                        <td>번호</td>
+                        <td>제목</td>
+                        <td>글쓴이</td>
+                        <td>날짜</td>
+                        <td>조회</td>
+                    </tr>
+                    <tr>
+                        <td>공지</td>
+                        <td>[이용안내] 회원가입 안내입니다.</td>
+                        <td>[셔틀버스안내] 셔틀버스 운행 안내</td>
+                        <td>2022-04-14</td>
+                        <td>10</td>
+                    </tr>
+                    <tr>
+                        <td>공지</td>
+                        <td></td>
+                        <td></td>
+                        <td>2022-04-14</td>
+                        <td>10</td>
+                    </tr>
+                    <tr>
+                        <td>공지</td>
+                        <td></td>
+                        <td></td>
+                        <td>2022-04-14</td>
+                        <td>10</td>
+                    </tr>
+                    <tr>
+                        <td>공지</td>
+                        <td></td>
+                        <td></td>
+                        <td>2022-04-14</td>
+                        <td>10</td>
+                    </tr>
+                    <tr>
+                        <td>공지</td>
+                        <td></td>
+                        <td></td>
+                        <td>2022-04-14</td>
+                        <td>100</td>
+                    </tr>
+                    <tr>
+                        <td>공지</td>
+                        <td></td>
+                        <td></td>
+                        <td>2022-04-14</td>
+                        <td>10</td>
+                    </tr>
+                    <tr>
+                        <td>공지</td>
+                        <td></td>
+                        <td></td>
+                        <td>2022-04-14</td>
+                        <td>100</td>
+                    </tr>
+                    <tr>
+                        <td>공지</td>
+                        <td></td>
+                        <td></td>
+                        <td>2022-04-14</td>
+                        <td>10</td>
+                    </tr>
+                    <tr>
+                        <td>공지</td>
+                        <td></td>
+                        <td></td>
+                        <td>2022-04-14</td>
+                        <td>10</td>
+                    </tr>
+                    <tr>
+                        <td>공지</td>
+                        <td></td>
+                        <td></td>
+                        <td>2022-04-14</td>
+                        <td>10</td>
+                    </tr>
+                    <tr>
+                        <td>공지</td>
+                        <td></td>
+                        <td></td>
+                        <td>2022-04-14</td>
+                        <td>1</td>
+                    </tr>
+                </table>
+            </div>
             
         </section>
     </main>
