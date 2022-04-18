fcfeea054bc5f9aa9b364d3e81c23e18acc9827a
Kimsonyon <ksonyong1109@gmail.com>
2022-04-18 11:29:42 +0900

    메인페이지 링크(서브페이지) 메뉴 작성
    메인 페이지 작성

---
+++ location.php
@@ -0,0 +1,37 @@
+<?php
+require_once 'includes/init.php';
+require_once 'includes/start.php';
+?>
+<!-- HTML START -->
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <?=$head?>
+    <link rel="stylesheet" href="styles/location.css">
+    <!-- js 추가 -->
+    <!-- <script src="scripts/top.js"></script> -->
+</head>
+<body>
+    <header>
+        <?=$header?>
+    </header>
+    <main>
+        <section>
+            <nav class="tab_type1">
+                <ul>
+                    <li><a href="guide.php">이용안내</a></li>
+                    <li><a href="schedule.php">영화시간표</a></li>
+                    <li><a href="culture.php">문화예술</a></li>
+                    <li><a href="notice.php">공지사항</a></li>
+                    <li><a href="location.php">오시는길</a></li>
+                </ul>
+            </nav>
+
+            
+        </section>
+    </main>
+    <footer>
+        <?=$footer?>
+    </footer>
+</body>
+</html>
