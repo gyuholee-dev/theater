73a8ef63e379bf70d12b86531f3f279044221307
LeeGyuho <andrewgyuholee@gmail.com>
2022-04-18 16:33:50 +0900

    예약내역 진행중

--- mypage.php
+++ mypage.php
@@ -14,13 +14,14 @@ $myPoints = $userData['points'];
 $sql = "SELECT ticket.*,
 movie.firmtitle AS firmtitle,
 movie.topimg AS topimg,
+movie.summary AS summary,
 screen.scrnstart AS scrnstart,
 screen.scrnend AS scrnend
 FROM theater_ticket AS ticket
 JOIN theater_movie AS movie ON ticket.firmcode = movie.firmcode
-JOIN theater_screen AS screen ON ticket.firmcode = screen.firmcode 
-AND ticket.scrnnum = screen.scrnnum AND ticket.scrncnt = screen.scrncnt
+JOIN theater_screen AS screen ON ticket.scrncode = screen.scrncode 
 WHERE ticket.userid = '$userId' ";
+$res = mysqli_query($DB, $sql);
 
 ?>
 <!-- HTML START -->
@@ -28,25 +29,52 @@ WHERE ticket.userid = '$userId' ";
 <html lang="ko">
 <head>
     <?=$head?>
-    <!-- css 추가 -->
-    <!-- <link rel="stylesheet" href="styles/top.css"> -->
-    <!-- js 추가 -->
-    <!-- <script src="scripts/top.js"></script> -->
+    <link rel="stylesheet" href="styles/mypage.css">
 </head>
 <body>
     <header>
         <?=$header?>
     </header>
     <main>
-    <!-- 여기에 컨텐츠를 추가
-        <section id="sectionId">
-            <div class="class type">
-                내용
+        <section id="purchased">
+            <div class="title">예약내역</div>
+            <div class="content">
+            <?php
+                $html = '';
+                while ($data = mysqli_fetch_assoc($res)) {
+
+                    $codes = decode_scrncode($data['scrncode']);
+                    $screenInfo = $codes['scrndate'].', '.
+                        $codes['scrnnum'].'관 '.$codes['scrncnt'].'회차, '.
+                        $data['scrnstart'].' ~ '.$data['scrnend'].', 좌석: '.$data['seatnum'];
+
+                    $paydate = date('Y-m-d H:i:s', $data['paydate']);
+                    $payment = number_format($data['payment']).'원';
+
+                    $html .= "
+                        <div class='ticket'>
+                            <div class='topimg'>
+                                <img src='$data[topimg]'>
+                            </div>
+                            <div class='info'>
+                                <div class='title'>$data[firmtitle]</div>
+                                <div class='summary'>$data[summary]</div>
+                                <div class='screen'>
+                                    $screenInfo
+                                </div>
+                            </div>
+                            <div class='payment'>
+                                <div class='date'>$paydate</div>
+                                <div class='points'>$payment</div>
+                                <input type='button' value='예약취소'>
+                            </div>
+                        </div>
+                    ";
+                }
+                echo $html;
+            ?>
             </div>
-            ...
         </section> 
-        ...
-    -->
     </main>
     <footer>
         <?=$footer?>
